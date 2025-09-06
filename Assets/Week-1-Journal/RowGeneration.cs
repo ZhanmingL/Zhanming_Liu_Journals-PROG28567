cb7f63a3376bb0e44f8dcf2d3858b8c7b5851523
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-06 19:09:22 -0400

    Task 2 all done
    Now, I'm going to continue writing journal. After that, I will spend last 45 minutes working on task3.

--- Assets/Week-1-Journal/RowGeneration.cs
+++ Assets/Week-1-Journal/RowGeneration.cs
@@ -4,34 +4,34 @@ using TMPro;
 
 public class RowGeneration : MonoBehaviour
 {
+    //claim
     public Button generate;
     public TMP_InputField squaresNumInput;
 
     float squareLeftTop; //Each spawned square's top left point's x value. Used for getting other 3 points as well.
-    int length = 2; //This time is full side length, not half.
+    int length = 2; //This time is full side length, not half. Each side length of square is 2.
 
-    void Update()
-    {
 
-        
-
-    }
-
+    //When button clicks
     public void GenerateRow()
     {
-        int rowNumber = int.Parse(squaresNumInput.text);
+        int rowNumber = int.Parse(squaresNumInput.text); //Transfer Input text value to int variable value.
+
+        Debug.Log(rowNumber); //Check whether squareNumInput's value is valid or not.
 
         Vector2 ScreenLeftPos = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height / 2));
+        //World's coordinate stands for left point, y value is in the middle.
         
         for(int i = 0; i < rowNumber; i++)
         {
-            squareLeftTop = ScreenLeftPos.x + i;
+            squareLeftTop = ScreenLeftPos.x + i; //Each new spawned square's topleft x value moves i unit to the right. So it achieves side by side.
 
+            //In each of my for loop, get four points of each square.
             Vector2 leftTop = new Vector2(squareLeftTop, ScreenLeftPos.y);
             Vector2 rightTop = new Vector2(squareLeftTop + length, ScreenLeftPos.y);
-            Vector2 leftBottom = new Vector2(squareLeftTop, ScreenLeftPos.y - length/2);
+            Vector2 leftBottom = new Vector2(squareLeftTop, ScreenLeftPos.y - length/2); //use half of length to get bottom points' y values.
             Vector2 rightBottom = new Vector2(squareLeftTop + length, ScreenLeftPos.y - length/2);
-
+            //Draw squares and keep 5 seconds.
             Debug.DrawLine(leftTop, rightTop, Color.white, 5);
             Debug.DrawLine(rightTop, rightBottom, Color.white, 5);
             Debug.DrawLine(rightBottom, leftBottom, Color.white, 5);
