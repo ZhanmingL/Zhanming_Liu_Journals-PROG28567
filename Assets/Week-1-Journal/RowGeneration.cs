85750cee6738a45e59709961f58ff7d074455f5a
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-06 18:48:25 -0400

    Time's Up (3rd 45 mins)
    I almost done Task2's a & b, squares are generated side by side. Next step, I gonna quickly finish task2 c and finish my comments. Then spend last 45 minutes doing task3.

--- Assets/Week-1-Journal/RowGeneration.cs
+++ Assets/Week-1-Journal/RowGeneration.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RowGeneration : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Button generate;
+    public TMP_InputField squaresNumInput;
 
-    // Update is called once per frame
+    float squareLeftTop; //Each spawned square's top left point's x value. Used for getting other 3 points as well.
+    int length = 2; //This time is full side length, not half.
+
     void Update()
     {
+
         
+
+    }
+
+    public void GenerateRow()
+    {
+        int rowNumber = int.Parse(squaresNumInput.text);
+
+        Vector2 ScreenLeftPos = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height / 2));
+        
+        for(int i = 0; i < rowNumber; i++)
+        {
+            squareLeftTop = ScreenLeftPos.x + i;
+
+            Vector2 leftTop = new Vector2(squareLeftTop, ScreenLeftPos.y);
+            Vector2 rightTop = new Vector2(squareLeftTop + length, ScreenLeftPos.y);
+            Vector2 leftBottom = new Vector2(squareLeftTop, ScreenLeftPos.y - length/2);
+            Vector2 rightBottom = new Vector2(squareLeftTop + length, ScreenLeftPos.y - length/2);
+
+            Debug.DrawLine(leftTop, rightTop, Color.white, 5);
+            Debug.DrawLine(rightTop, rightBottom, Color.white, 5);
+            Debug.DrawLine(rightBottom, leftBottom, Color.white, 5);
+            Debug.DrawLine(leftBottom, leftTop, Color.white, 5);
+        }
     }
 }
