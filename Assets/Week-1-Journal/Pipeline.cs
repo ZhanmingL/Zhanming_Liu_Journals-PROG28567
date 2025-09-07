e935396e1947213b63892e0ae8e2c733b4f3a754
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-06 20:21:07 -0400

    First two points can form a line.
    I have a first point and many second points. They can form lines per 0.2 second, but this is a bug, every line is connecting from middle point.

--- Assets/Week-1-Journal/Pipeline.cs
+++ Assets/Week-1-Journal/Pipeline.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
+    float time = 0;
+    Vector2 mousePos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,25 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            time = 0;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            time += Time.deltaTime;
+            if(time >= 0.1f)
+            {
+                Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                time = 0;
+
+                Debug.DrawLine(mousePos, newPos, Color.white, 5);
+            }
+            
+
+        }
     }
 }
