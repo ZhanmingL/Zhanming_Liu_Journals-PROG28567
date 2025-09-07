a33980eafad27c9c3c4d003edf75de87e98297ff
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-06 21:51:08 -0400

    All 3 tasks done
    All 3 tasks are finished, I tried my best as I can. I will finish my journal and submit later.

--- Assets/Week-1-Journal/Pipeline.cs
+++ Assets/Week-1-Journal/Pipeline.cs
@@ -2,23 +2,18 @@ using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
-    float time = 0;
+    float time = 0; //Timer
 
-    Vector2 mousePos;
-    Vector2 pastPos;
+    Vector2 mousePos; //Initial point when mouse clicked
+    Vector2 pastPos; //new point's previous point
+    Vector2 counting; //Calculate generated point's vectors
 
-    bool firstDraw = true;
+    bool firstDraw = true; //Check if it's clicking mouse or holding mouse.
+    //Helps check first frame; Helps check the connecting of first point and second point.
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        //Check the first click of the mouse: first frame when mouse clicked
         if (Input.GetMouseButtonDown(0) && firstDraw == true)
         {
             time = 0;
@@ -29,34 +24,41 @@ public class Pipeline : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             time += Time.deltaTime;
-
+            //The first connection (original mouse position and first generated point)
             if (firstDraw == false)
             {
                 Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                Debug.DrawLine(mousePos, newPos, Color.white, 5);
+                Debug.DrawLine(mousePos, newPos, Color.white, 60);
+                counting += mousePos + newPos; //Calculate length.
 
-                pastPos = newPos;
+                pastPos = newPos; //Change this point to NEXT NEW POINT's previous point
 
-                time = 0;
+                time = 0; //Re-load the timer
 
-                firstDraw = true;
+                firstDraw = true; //Turn it to true so that it will not run this if loop anymore.
             }
 
-            if (firstDraw == true)
+            if (firstDraw == true) //Check all generated points from 2nd point.
             {
                 if (time >= 0.1f)
                 {
                     Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                    Debug.DrawLine(pastPos, newPos, Color.white, 5);
+                    Debug.DrawLine(pastPos, newPos, Color.white, 60);
+                    counting += pastPos + newPos;
 
-                    pastPos = newPos;
+                    pastPos = newPos; //The same meaning, change new point to next new one's previous one.
 
                     time = 0;
                 }
             }
 
+            if (Input.GetMouseButtonUp(0))
+            { //When up my mouse, count sum of total length.
+                float magnitude = Mathf.Sqrt(counting.x * counting.x + counting.y * counting.y);
+                Debug.Log(magnitude);
+            }
         }
     }
 }
