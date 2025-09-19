a575ce8d574629197e381ec938768c99e742d7ba
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-19 19:19:08 -0400

    Task3 done
    I spent about 1 hour. Now I'm going to continue writing journal.

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
+    float moveSpeed = 1f; //I don't want to enter 20 times in the inspector, so I make it private here.
     public float arrivalDistance;
     public float maxFloatDistance;
 
-    // Start is called before the first frame update
+    Vector2 randomPoint;
+
     void Start()
     {
+        GenerateRandomPoint();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        MoveAsteroid();
+    }
+
+    public void MoveAsteroid()
+    {
+        maxFloatDistance = Vector3.Distance(transform.position, randomPoint);
+        
+
+        Debug.Log(maxFloatDistance);
+
+        if (maxFloatDistance > arrivalDistance)
+        {
+            Vector3 direction = (Vector3)randomPoint - transform.position;
+            transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            GenerateRandomPoint();
+        }
+        
+        
+    }
+
+    public void GenerateRandomPoint()
+    {
+        float x = Random.Range(0, Screen.width);
+        float y = Random.Range(0, Screen.height);
+        randomPoint = Camera.main.ScreenToWorldPoint(new Vector2(x, y));
     }
 }
