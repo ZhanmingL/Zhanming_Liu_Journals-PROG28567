9bbfc8b839630eeaa7f249c7f8aed119068b3e06
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-20 14:23:40 -0400

    Doing Task4, now facing an issue that only draw have a line

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -7,23 +7,37 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
-
-    // Update is called once per frame
+    [Range(0, 0.5f)]
+    public float t;
+    public AnimationCurve curve;
     void Update()
     {
+        DrawConstellation(0);
 
-
-        for (int i = 0; i < starTransforms.Count - 1; i++)
-        {
-            DrawConstellation(i, 0);
-        }
+        //for (int i = 0; i < starTransforms.Count - 1; i++)
+        //{
+        //   DrawConstellation(i, 0);
+        //}
     }
 
 
-    public void DrawConstellation(int order, float length)
+    public void DrawConstellation(int i)
     {
-        Debug.DrawLine(starTransforms[order].position, starTransforms[order + 1].position +
-            ((starTransforms[order + 1].position - starTransforms[order + 1].position).normalized * length), Color.white, 3f);
-        length += Time.deltaTime;
+        //Debug.DrawLine(starTransforms[order].position, (starTransforms[order + 1].position +
+        //   ((starTransforms[order + 1].position - starTransforms[order + 1].position).normalized) * length), Color.white, 3f);
+
+        Vector3 addLength = Vector3.Lerp(starTransforms[i].position, starTransforms[i + 1].position, t);
+
+        if (t < drawingTime)
+        {
+            t += Time.deltaTime;
+        }
+        else
+        {
+            t = 0;
+
+        }
+
+        Debug.DrawLine(starTransforms[i].position, addLength, Color.white);
     }
 }
