261205f43e82a416e1135145a014e896d990d27a
ZhanmingL <liuzhanm@sheridancollege.ca>
2025-09-20 14:46:57 -0400

    Task3 done
    Now I'm going to do comments and finish journal.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,13 +6,15 @@ using UnityEngine.UIElements;
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
-    public float drawingTime;
-    [Range(0, 0.5f)]
+    float drawingTime = 1;
+
+    int i = 0;
+    //[Range(0, 1)]
     public float t;
-    public AnimationCurve curve;
+
     void Update()
     {
-        DrawConstellation(0);
+        DrawConstellation();
 
         //for (int i = 0; i < starTransforms.Count - 1; i++)
         //{
@@ -21,11 +23,11 @@ public class Stars : MonoBehaviour
     }
 
 
-    public void DrawConstellation(int i)
+    public void DrawConstellation()
     {
         //Debug.DrawLine(starTransforms[order].position, (starTransforms[order + 1].position +
         //   ((starTransforms[order + 1].position - starTransforms[order + 1].position).normalized) * length), Color.white, 3f);
-
+        
         Vector3 addLength = Vector3.Lerp(starTransforms[i].position, starTransforms[i + 1].position, t);
 
         if (t < drawingTime)
@@ -35,7 +37,12 @@ public class Stars : MonoBehaviour
         else
         {
             t = 0;
+            i++;
+        }
 
+        if(i == starTransforms.Count - 1)
+        {
+            i = 0;
         }
 
         Debug.DrawLine(starTransforms[i].position, addLength, Color.white);
